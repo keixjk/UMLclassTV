fe9ff75482d07f94174f9e3505f8d24f84f7ef7c
keixjk <kayegallogo@gmail.com>
2023-05-20 01:16:35 +0800

    initialized TV objects, yes!

---
+++ UMLclassTV.py
@@ -0,0 +1,34 @@
+# TV class definition
+class TV:
+# initialize TV objects
+    def __init__(self):
+        self.channel = 1
+        self.volumeLevel = 1
+        self.on = False
+   
+
+# set on to True
+
+# set on to False
+# return the current channel
+# if on is True and channel is between 1 and 120 (inclusive):
+    # set the channel to the given value
+# return the current volume level
+# if on is True and channel is between 1 and 7 (inclusive):
+    # set the volume level to the given value
+# if on is True and channel is less than 120:
+    # increment the channel by 1
+# if on is True and channel is greater tahn 1:
+    # decrement the channel by 1
+# if on is True and volumeLevel is less than 7:
+    # increment the volume level by 1
+# if on is True and volumeLevel is greater than 1:
+    # decrement the volume level by 1
+
+# TestTV Program
+# Create two TV objects
+# Turn on TV1 and set the channel and volume level
+# print
+# Turn on TV2 and set the channel and volume level
+# print
+# Run the TestTV program
